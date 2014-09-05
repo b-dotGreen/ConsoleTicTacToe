3b5d58b225c6f92ca23606af475b9b5dd1eab1ed
GeekBGreen <beatbox1029@gmail.com>
2014-09-04 21:46:13 -0400

    - Console Tic-Tac-Toe.ncp   + Updated with design changes made during coding
    - Implemented Player.cs

--- ConsoleTicTacToe/Player.cs
+++ ConsoleTicTacToe/Player.cs
@@ -7,5 +7,74 @@ namespace ConsoleTicTacToe
 {
     class Player
     {
+        private string name;
+        private string symbol;
+        private int playerNumber;
+        
+        // Gets and sets the name of the player
+        public string Name
+        {
+            get { return name; }
+
+            set { name = value; }
+        }
+
+        // Gets and sets the symbol the player is using to mark the board
+        public string Symbol
+        {
+            get { return symbol; }
+
+            set { symbol = value; }
+        }
+
+        // Gets and sets the player's number in the game (e.g. Payer 1, Player 2)
+        public int PlayerNumber
+        {
+            get { return playerNumber; }
+
+            set { playerNumber = value; }
+        }
+
+        // Method that makrs the GameBoard at the desired position. Returns true if move successfully place on the board.
+        public bool MarkBoard(GameBoard board, int position)
+        {
+            string[] marks = board.CurrentLayout.Split('|');
+
+            for (int i=0; i < marks.Length; i++)
+            {
+                if (position == Int32.Parse(marks[i]))
+                {
+                    marks[i] = symbol;
+
+                    ReconstructLayout(board, marks);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+       // Method that reconstructs the board's layout from an array of marks to the string of marks delimited by the pipe character.
+       // Returns the board's layout after modified.
+       private string ReconstructLayout(GameBoard board, string[] marks)
+        {
+           board.CurrentLayout = "";
+           string lastPostion = marks.Last();
+
+           foreach (string s in marks)
+           {
+               if (s.Equals(lastPostion))
+               {
+                   board.CurrentLayout += (s);
+               }
+               else
+               {
+                   board.CurrentLayout += (s + "|");
+               }
+           }
+
+           return board.CurrentLayout;
+        }
+
     }
 }
